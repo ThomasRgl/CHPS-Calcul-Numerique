bad84f87fcfe87368f2dc19b35c77a5b5be28355
ThomasRgl <thomasroglin@free.fr>
2023-01-05 10:51:20 +0100

    reindentation with tab 4

--- src/lib_iter_poisson1D.c
+++ src/lib_iter_poisson1D.c
@@ -7,36 +7,24 @@
 #include "lib_poisson1D.h"
 #include <cblas.h>
 
+void eig_poisson1D(double *eigval, int *la) {}
 
-void eig_poisson1D(double* eigval, int *la){
-}
+double eigmax_poisson1D(int *la) { return 0; }
 
-double eigmax_poisson1D(int *la){
-  return 0;
-}
+double eigmin_poisson1D(int *la) { return 0; }
 
-double eigmin_poisson1D(int *la){
-  return 0;
-}
+double richardson_alpha_opt(int *la) { return 0; }
 
-double richardson_alpha_opt(int *la){
-  return 0;
-}
+void richardson_alpha(double *AB, double *RHS, double *X, double *alpha_rich,
+                      int *lab, int *la, int *ku, int *kl, double *tol,
+                      int *maxit, double *resvec, int *nbite) {}
 
-void richardson_alpha(double *AB, double *RHS, double *X, double *alpha_rich, int *lab, int *la,int *ku, int*kl, double *tol, int *maxit, double *resvec, int *nbite){
+void extract_MB_jacobi_tridiag(double *AB, double *MB, int *lab, int *la,
+                               int *ku, int *kl, int *kv) {}
 
-}
+void extract_MB_gauss_seidel_tridiag(double *AB, double *MB, int *lab, int *la,
+                                     int *ku, int *kl, int *kv) {}
 
-void extract_MB_jacobi_tridiag(double *AB, double *MB, int *lab, int *la,int *ku, int*kl, int *kv){
-
-}
-
-void extract_MB_gauss_seidel_tridiag(double *AB, double *MB, int *lab, int *la,int *ku, int*kl, int *kv){
-
-}
-
-void richardson_MB(double *AB, double *RHS, double *X, double *MB, int *lab, int *la,int *ku, int*kl, double *tol, int *maxit, double *resvec, int *nbite){
-
-}
-
-
+void richardson_MB(double *AB, double *RHS, double *X, double *MB, int *lab,
+                   int *la, int *ku, int *kl, double *tol, int *maxit,
+                   double *resvec, int *nbite) {}
--- src/tp_poisson1D_iter.c
+++ src/tp_poisson1D_iter.c
@@ -3,108 +3,110 @@
 /* This file contains the main function   */
 /* to solve the Poisson 1D problem        */
 /******************************************/
-#include "lib_poisson1D.h"
 #include "atlas_headers.h"
+#include "lib_poisson1D.h"
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 /* ** argc: Number of arguments */
 /* ** argv: Values of arguments */
 {
-  int ierr;
-  int jj;
-  int nbpoints, la;
-  int ku, kl, lab, kv;
-  int *ipiv;
-  int info;
-  int NRHS;
-  double T0, T1;
-  double *RHS, *SOL, *EX_SOL, *X;
-  double *AB;
-  double *MB;
-  
-  double temp, relres;
+    int ierr;
+    int jj;
+    int nbpoints, la;
+    int ku, kl, lab, kv;
+    int *ipiv;
+    int info;
+    int NRHS;
+    double T0, T1;
+    double *RHS, *SOL, *EX_SOL, *X;
+    double *AB;
+    double *MB;
 
-  double opt_alpha;
+    double temp, relres;
 
-  /* Size of the problem */
-  NRHS=1;
-  nbpoints=12;
-  la=nbpoints-2;
+    double opt_alpha;
 
-  /* Dirichlet Boundary conditions */
-  T0=5.0;
-  T1=20.0;
+    /* Size of the problem */
+    NRHS = 1;
+    nbpoints = 12;
+    la = nbpoints - 2;
 
-  printf("--------- Poisson 1D ---------\n\n");
-  RHS=(double *) malloc(sizeof(double)*la);
-  SOL=(double *) calloc(la, sizeof(double)); 
-  EX_SOL=(double *) malloc(sizeof(double)*la);
-  X=(double *) malloc(sizeof(double)*la);
+    /* Dirichlet Boundary conditions */
+    T0 = 5.0;
+    T1 = 20.0;
 
-  /* Setup the Poisson 1D problem */
-  /* General Band Storage */
-  set_grid_points_1D(X, &la);
-  set_dense_RHS_DBC_1D(RHS,&la,&T0,&T1);
-  set_analytical_solution_DBC_1D(EX_SOL, X, &la, &T0, &T1);
-  
-  write_vec(RHS, &la, "data/RHS.dat");
-  write_vec(EX_SOL, &la, "data/EX_SOL.dat");
-  write_vec(X, &la, "data/X_grid.dat");
+    printf("--------- Poisson 1D ---------\n\n");
+    RHS = (double *)malloc(sizeof(double) * la);
+    SOL = (double *)calloc(la, sizeof(double));
+    EX_SOL = (double *)malloc(sizeof(double) * la);
+    X = (double *)malloc(sizeof(double) * la);
 
-  kv=0;
-  ku=1;
-  kl=1;
-  lab=kv+kl+ku+1;
-  
-  AB = (double *) malloc(sizeof(double)*lab*la);
-  set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
-  
-  /* uncomment the following to check matrix A */
-  // write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "data/AB.dat");
-  
-  /********************************************/
-  /* Solution (Richardson with optimal alpha) */
+    /* Setup the Poisson 1D problem */
+    /* General Band Storage */
+    set_grid_points_1D(X, &la);
+    set_dense_RHS_DBC_1D(RHS, &la, &T0, &T1);
+    set_analytical_solution_DBC_1D(EX_SOL, X, &la, &T0, &T1);
 
-  /* Computation of optimum alpha */
-  opt_alpha = richardson_alpha_opt(&la);
-  printf("Optimal alpha for simple Richardson iteration is : %lf",opt_alpha); 
+    write_vec(RHS, &la, "data/RHS.dat");
+    write_vec(EX_SOL, &la, "data/EX_SOL.dat");
+    write_vec(X, &la, "data/X_grid.dat");
 
-  /* Solve */
-  double tol=1e-3;
-  int maxit=1000;
-  double *resvec;
-  int nbite=0;
+    kv = 0;
+    ku = 1;
+    kl = 1;
+    lab = kv + kl + ku + 1;
 
-  resvec=(double *) calloc(maxit, sizeof(double));
+    AB = (double *)malloc(sizeof(double) * lab * la);
+    set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
 
-  /* Solve with Richardson alpha */
-  richardson_alpha(AB, RHS, SOL, &opt_alpha, &lab, &la, &ku, &kl, &tol, &maxit, resvec, &nbite);
+    /* uncomment the following to check matrix A */
+    // write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "data/AB.dat");
 
-  /* Richardson General Tridiag */
+    /********************************************/
+    /* Solution (Richardson with optimal alpha) */
 
-  /* get MB (:=M, D for Jacobi, (D-E) for Gauss-seidel) */
-  kv = 1;
-  ku = 1;
-  kl = 1;
-  MB = (double *) malloc(sizeof(double)*(lab)*la);
-  // extract_MB_jacobi_tridiag(AB, MB, &lab, &la, &ku, &kl, &kv);
-  // extract_MB_gauss_seidel_tridiag(AB, MB, &lab, &la, &ku, &kl, &kv);
-  
-  /* Solve with General Richardson */
-  // richardson_MB(AB, RHS, SOL, MB, &lab, &la, &ku, &kl, &tol, &maxit, resvec, &nbite);
-  
-  /* Write solution */
-  write_vec(SOL, &la, "data/SOL.dat");
+    /* Computation of optimum alpha */
+    opt_alpha = richardson_alpha_opt(&la);
+    printf("Optimal alpha for simple Richardson iteration is : %lf", opt_alpha);
 
-  /* Write convergence history */
-  write_vec(resvec, &nbite, "data/RESVEC.dat");
+    /* Solve */
+    double tol = 1e-3;
+    int maxit = 1000;
+    double *resvec;
+    int nbite = 0;
 
-  free(resvec);
-  free(RHS);
-  free(SOL);
-  free(EX_SOL);
-  free(X);
-  free(AB);
-  free(MB);
-  printf("\n\n--------- End -----------\n");
+    resvec = (double *)calloc(maxit, sizeof(double));
+
+    /* Solve with Richardson alpha */
+    richardson_alpha(AB, RHS, SOL, &opt_alpha, &lab, &la, &ku, &kl, &tol,
+                     &maxit, resvec, &nbite);
+
+    /* Richardson General Tridiag */
+
+    /* get MB (:=M, D for Jacobi, (D-E) for Gauss-seidel) */
+    kv = 1;
+    ku = 1;
+    kl = 1;
+    MB = (double *)malloc(sizeof(double) * (lab)*la);
+    // extract_MB_jacobi_tridiag(AB, MB, &lab, &la, &ku, &kl, &kv);
+    // extract_MB_gauss_seidel_tridiag(AB, MB, &lab, &la, &ku, &kl, &kv);
+
+    /* Solve with General Richardson */
+    // richardson_MB(AB, RHS, SOL, MB, &lab, &la, &ku, &kl, &tol, &maxit,
+    // resvec, &nbite);
+
+    /* Write solution */
+    write_vec(SOL, &la, "data/SOL.dat");
+
+    /* Write convergence history */
+    write_vec(resvec, &nbite, "data/RESVEC.dat");
+
+    free(resvec);
+    free(RHS);
+    free(SOL);
+    free(EX_SOL);
+    free(X);
+    free(AB);
+    free(MB);
+    printf("\n\n--------- End -----------\n");
 }
